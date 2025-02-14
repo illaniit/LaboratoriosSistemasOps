ed6466ce5fd36123771440dc2a358c0d56d1f30d
Illán <86691041+illaniit@users.noreply.github.com>
2025-02-14 10:39:13 +0100

    Add files via upload

---
+++ scr_ph_01_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>  // Para getpid()
+
+// Esta es la función que el hilo ejecutará
+void* printMessages(void* arg) {
+    char* message = (char*)arg;
+    printf("Hilo en ejecución: PID del proceso: %d, TID del hilo: %lu\n", getpid(), pthread_self());
+    printf("%s\n", message);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1;
+
+    // Mostrar el PID del proceso principal
+    printf("Proceso principal: PID del proceso: %d\n", getpid());
+
+    // Crear el hilo, pasando una cadena de texto como argumento para que la imprima
+    if (pthread_create(&thread1, NULL, printMessages, "Hola, soy un hilo ejecutándome.") != 0) {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+
+    // Esperar a que el hilo termine
+    if (pthread_join(thread1, NULL) != 0) {
+        perror("pthread_join");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
